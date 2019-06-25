06cc4b8586becb5f90bab3bbae838c664e916e00
Scott Peterson <scott.peterson@unity3d.com>
2019-06-25 10:40:06 +0200

    Added class to create Barracuda model of OIDN, as well as read the weights from a JSON file

---
+++ Assets/OIDNModel.cs
@@ -0,0 +1,193 @@
+using System.Collections.Generic;
+using Barracuda;
+using UnityEngine;
+
+class OIDNModel
+{
+	#pragma warning disable CS0649
+	[System.Serializable]
+	public class JSONTestSet
+	{
+		public JSONTensor[] inputs;
+		// public JSONTensor[] outputs;
+		public JSONTensor GetInputByName(string name) 
+		{
+			foreach (var input in inputs)
+			{
+				if (input.name == name) return input;
+			}
+			return null;
+		}
+	}
+
+	[System.Serializable]
+	public class JSONTensor
+	{
+		public string name;
+		public int[] shape;
+		public string type;
+		public float[] data;
+	}
+	#pragma warning restore CS0649
+
+
+	public static Model BuildModel(TextAsset weightsJSON, int height, int width)
+	{
+		Model res = new Model();
+		// var json = System.File.ReadAllText(fullpath);
+		JSONTestSet testSet = JsonUtility.FromJson<JSONTestSet>(weightsJSON.text);
+
+		res.inputs = new Model.Input [] {
+			new Model.Input { name = "input" , shape = new int[] { -1, height, width, 3 } }
+		};
+
+		List<Layer> layers = new List<Layer>();
+
+		Layer conv1 = AddConvolution(layers, "conv1", false, true, testSet, res.inputs[0].name);
+		Layer conv1b = AddConvolution(layers, "conv1b", false, true, testSet, conv1.name);
+		Layer pool1 = AddPool(layers, "pool1", conv1b.name);
+		Layer conv2 = AddConvolution(layers, "conv2", false, true, testSet, pool1.name);
+		Layer pool2 = AddPool(layers, "pool2", conv2.name);
+		Layer conv3 = AddConvolution(layers, "conv3", false, true, testSet, pool2.name);
+		Layer pool3 = AddPool(layers, "pool3", conv3.name);	
+		Layer conv4 = AddConvolution(layers, "conv4", false, true, testSet, pool3.name);
+		Layer pool4 = AddPool(layers, "pool4", conv4.name);	
+		Layer conv5 = AddConvolution(layers, "conv5", false, true, testSet, pool4.name);
+		Layer pool5 = AddPool(layers, "pool5", conv5.name);
+
+		Layer upsample4 = AddUpsample(layers, "upsample4", pool5.name);
+		Layer concat4 = AddConcatonate(layers, "concat4", upsample4.name, pool4.name);
+		Layer conv6 = AddConvolution(layers, "conv6", true, true, testSet, concat4.name);
+		Layer conv6b = AddConvolution(layers, "conv6b", true, true, testSet, conv6.name);
+
+		Layer upsample3 = AddUpsample(layers, "upsample3", conv6b.name);
+		Layer concat3 = AddConcatonate(layers, "concat3", upsample3.name, pool3.name);
+		Layer conv7 = AddConvolution(layers, "conv7", true, true, testSet, concat3.name);
+		Layer conv7b = AddConvolution(layers, "conv7b", true, true, testSet, conv7.name);
+
+		Layer upsample2 = AddUpsample(layers, "upsample2", conv7b.name);
+		Layer concat2 = AddConcatonate(layers, "concat2", upsample2.name, pool2.name);
+		Layer conv8 = AddConvolution(layers, "conv8", true, true, testSet, concat2.name);
+		Layer conv8b = AddConvolution(layers, "conv8b", true, true, testSet, conv8.name);
+
+		Layer upsample1 = AddUpsample(layers, "upsample1", conv8b.name);
+		Layer concat1 = AddConcatonate(layers, "concat1", upsample1.name, pool1.name);
+		Layer conv9 = AddConvolution(layers, "conv9", true, true, testSet, concat1.name);
+		Layer conv9b = AddConvolution(layers, "conv9b", true, true, testSet, conv9.name);
+
+		Layer upsample0 = AddUpsample(layers, "upsample0", conv9b.name);
+		Layer concat0 = AddConcatonate(layers, "concat0", upsample0.name, res.inputs[0].name);
+		Layer conv10 = AddConvolution(layers, "conv10", true, true, testSet, concat0.name);
+		Layer conv10b = AddConvolution(layers, "conv10b", true, true, testSet, conv10.name);
+
+		Layer conv11 = AddConvolution(layers, "conv11", true, false, testSet, conv10b.name);
+
+		res.layers = layers.ToArray();
+		res.memories = new Model.Memory[0];
+
+		res.outputs = new string[] { conv11.name };
+
+		return res;
+	}
+
+	
+
+	// shape is read from file as Height, Width, Channels, Kernels 
+	public static Layer AddConvolution(List<Layer> layers, string name, bool isTranspose, bool addRelu, JSONTestSet testSet, string inputName)
+	{
+		Layer layer = new Layer();
+
+		JSONTensor weightTensor = testSet.GetInputByName(name + "/W");
+		JSONTensor biasTensor = testSet.GetInputByName(name + "/b");
+		if (weightTensor == null || biasTensor == null)
+		{
+			Debug.LogError("weight or bias with base name " + name + " could not be found");
+			return null;
+		}
+
+		layer.type = isTranspose ? Layer.Type.Conv2DTrans : Layer.Type.Conv2D;
+		layer.activation = Layer.Activation.None;
+		layer.name = name;
+		layer.datasets = new Layer.DataSet[2];
+		layer.weights = new float[weightTensor.data.Length + biasTensor.data.Length];
+		
+		layer.datasets[0].shape = new TensorShape(weightTensor.shape);
+		layer.datasets[0].offset = 0;
+		layer.datasets[0].length = weightTensor.data.Length;
+
+		int kernels = biasTensor.shape[biasTensor.shape.Length - 1];
+		layer.datasets[1].shape = new TensorShape(1, 1, 1, kernels);
+		layer.datasets[1].offset = weightTensor.data.Length;
+		layer.datasets[1].length = biasTensor.data.Length;
+
+		layer.stride = new[] {1, 1};
+		layer.pad = new[] {1, 1, 1, 1};
+		
+		layer.inputs  = new [] { inputName };
+		layers.Add(layer);
+
+		if (addRelu)
+		{
+			layer = AddRelu(layers, name + "Relu", layer.name);
+		}
+		return layer;
+	}
+
+	public static Layer AddRelu(List<Layer> layers, string name, string inputName)
+	{
+		Layer layer = new Layer();
+		layer.type = Layer.Type.Activation;
+		layer.activation = Layer.Activation.Relu;
+		layer.name = name;
+		layer.datasets = new Layer.DataSet[0];
+		layer.weights = new float[0];
+		layer.inputs = new [] {inputName};
+		layers.Add(layer);
+		return layer;
+	}
+
+	public static Layer AddPool(List<Layer> layers, string name, string inputName)
+	{
+		Layer layer = new Layer();
+		layer.type = Layer.Type.MaxPool2D;
+		layer.activation = Layer.Activation.None;
+		layer.name = name;
+		layer.datasets = new Layer.DataSet[0];
+		layer.weights = new float[0];
+		layer.pool = new [] {2, 2};
+		layer.stride = new[] {2, 2};
+		// TODO: check the proper pad in pooling
+		layer.pad = new[] {0, 0, 0, 0};
+		layer.inputs = new [] {inputName};
+		layers.Add(layer);
+		return layer;
+	}
+
+	public static Layer AddUpsample(List<Layer> layers, string name, string inputName)
+	{
+		Layer layer = new Layer();
+		layer.type = Layer.Type.Upsample2D;
+		layer.activation = Layer.Activation.None;
+		layer.name = name;
+		layer.datasets = new Layer.DataSet[0];
+		layer.weights = new float[0];
+		layer.pool = new [] {2, 2};
+		layer.inputs = new [] {inputName};
+		layers.Add(layer);
+		return layer;
+	}
+
+	public static Layer AddConcatonate(List<Layer> layers, string name, string inputA, string inputB)
+	{
+		Layer layer = new Layer();
+		layer.type = Layer.Type.Concat;
+		layer.activation = Layer.Activation.None;
+		layer.name = name;
+		layer.datasets = new Layer.DataSet[0];
+		layer.weights = new float[0];
+		layer.inputs = new [] {inputA, inputB};
+		layer.axis = -1;
+		layers.Add(layer);
+		return layer;
+	}
+}
--- Assets/RunTest.cs
+++ Assets/RunTest.cs
@@ -11,19 +11,21 @@ public class RunTest : MonoBehaviour
 
 	public RawImage displayImage;
 
+	public TextAsset weightsJSON;
+
 	private Model model;
 	private IWorker engine;
-	private Tensor input;
+	private Tensor inputTensor;
 
 	// Use this for initialization
 	void Start ()
 	{
 		Application.targetFrameRate = 60;
 		
-		model = BuildModel(inputImage.height, inputImage.width);
-		engine = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.ComputePrecompiled, model, false);
+		model = OIDNModel.BuildModel(weightsJSON, inputImage.height, inputImage.width);
+		engine = BarracudaWorkerFactory.CreateWorker(BarracudaWorkerFactory.Type.ComputePrecompiled, model, true);
 		
-		input = new Tensor(inputImage);
+		inputTensor = new Tensor(inputImage);
 
 		StartCoroutine(RunInference());
 	}
@@ -35,7 +37,7 @@ public class RunTest : MonoBehaviour
 		displayImage.texture = inputImage;
 
 		Profiler.BeginSample("Schedule execution");
-		engine.Execute(input);
+		engine.Execute(inputTensor);
 		Profiler.EndSample();
 
 
@@ -48,46 +50,6 @@ public class RunTest : MonoBehaviour
 
 
 		engine.Dispose();
-		input.Dispose();
-	}
-
-	Model BuildModel(int h, int w)
-	{
-		Model res = new Model();
-		
-		res.inputs = new Model.Input [] {
-			new Model.Input { name = "input" ,       shape = new int[] { -1, h, w, 3 } }
-		};
-		
-		Layer conv1 = new Layer();
-		
-		var W = new TensorShape(3, 3, 3, 32);
-		var B = new TensorShape(1, 1, 1, 32);
-		
-		conv1.type = Layer.Type.Conv2D;
-		conv1.activation = Layer.Activation.None;
-		conv1.name = "conv1";
-		conv1.datasets = new Layer.DataSet[2];
-		conv1.weights = new float[W.length + B.length]; // TODO: fill in
-		
-		conv1.datasets[0].shape = W;
-		conv1.datasets[0].offset = 0;
-		conv1.datasets[0].length = W.length;
-
-		conv1.datasets[1].shape = B;
-		conv1.datasets[1].offset = W.length;
-		conv1.datasets[1].length = B.length;
-
-		conv1.stride = new[] {1, 1};
-		conv1.pad = new[] {1, 1, 1, 1};
-		
-		conv1.inputs  = new [] { res.inputs[0].name };
-
-		res.layers = new[] {conv1};
-		res.memories = new Model.Memory[0];
-
-		res.outputs = new string[] { "conv1"};
-
-		return res;
+		inputTensor.Dispose();
 	}
 }
