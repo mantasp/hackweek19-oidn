66ac8fda1436309a32075a4462e28e63d00fcdf3
Rasmus Rønn Nielsen <rasmusn@unity3d.com>
2019-06-27 10:59:19 +0200

    Update API of C#-side denoiser

--- Assets/BarracudaDenoiser/BarracudaDenoiser.cs
+++ Assets/BarracudaDenoiser/BarracudaDenoiser.cs
@@ -3,21 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Though not strictly needed, we had to make a namespace to make the C++ -> C# invocation work.
 namespace BarracudaDenoiser
 {
     static public class API
     {
-        unsafe static public void Test(IntPtr ptr, int length)
+        unsafe static public void Denoise(Texture2D input, RenderTexture output)
         {
-            //a[0] = 123f;
-            //Debug.Log("Hello: " + a[1] + ", " + length);
-            //Debug.Log(" " + a);
-            int* a = (int*)ptr;
-            for (int x = 0; x < length; ++x)
-            {
-                Debug.Log(a[x]);
-            }
+            // write results to output
         }
     }
 }
